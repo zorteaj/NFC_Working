7a1c2ffc0593b724783819222fbace282f53fc2a
zorteaj <jzortea@gmail.com>
2018-04-06 10:55:14 -0400

    Notification created when FCM message arrives while app is in foreground

--- app/src/main/java/com/jzap/setlist/nfc/MyFirebaseMessagingService.java
+++ app/src/main/java/com/jzap/setlist/nfc/MyFirebaseMessagingService.java
@@ -1,5 +1,9 @@
 package com.jzap.setlist.nfc;
 
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -11,13 +15,38 @@ import com.google.firebase.messaging.RemoteMessage;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
     private static final String TAG = "JAZ_NFC";
+    private static final String CHANNEL_ID = "JZ_NFC_CHANNEL";
+    static int notificationId = 0;
+
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         // TODO: Handle FCM messages here.
         // If the application is in the foreground handle both data and notification messages here.
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated.
+
         Log.d(TAG, "From: " + remoteMessage.getFrom());
         Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
+
+        Intent intent = new Intent(this, ContactActivity.class);
+        intent.setAction("CONTACT_REQUEST");
+        intent.putExtra("REQUESTOR", remoteMessage.getData().get("REQUESTOR"));
+        // TODO: These are probably not the flags I want
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.greetlogo)
+                .setContentTitle(remoteMessage.getNotification().getTitle())
+                .setContentText(remoteMessage.getNotification().getBody())
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        // notificationId is a unique int for each notification that you must define
+        notificationManager.notify(notificationId++, mBuilder.build());
     }
 }
